9250dee620f12b4060298ea468b33f36e04d57f0
seherdemiroglu <117183782+seherdemiroglu@users.noreply.github.com>
2025-11-16 01:00:23 +0300

    product

--- ScienceMarket/Areas/Admin/Controllers/ProductsController.cs
+++ ScienceMarket/Areas/Admin/Controllers/ProductsController.cs
@@ -1,14 +1,172 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScienceMarketData;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp;
 
 namespace ScienceMarket.Areas.Admin.Controllers;
 
 [Area("Admin")]
 [Authorize(Roles = "Administrators")]
-public class ProductsController : Controller
+public class ProductsController (ScienceMarketDbContext dbContext): Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var items = await dbContext.Products.OrderBy(p => p.Name).ToListAsync();
+        return View(items);
+    }
+
+    public IActionResult Create()
+    {
+        return View(new Product { });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product model)
+    {
+        model.CreatedAt = DateTime.UtcNow;
+
+        var form = Request.Form;
+
+        var specs = await dbContext.Specifications.ToListAsync();
+
+        specs
+            .Where(p => !string.IsNullOrEmpty(form[p.Id.ToString()]))
+            .ToList()
+            .ForEach(p =>
+            {
+                model.Specs.Add(new ProductSpecification
+                {
+                    SpecificationId = p.Id,
+                    Value = form[p.Id.ToString()]
+                });
+            });
+
+
+        if (model.ImageFile is not null)
+        {
+            using var image = await Image.LoadAsync(model.ImageFile.OpenReadStream());
+            image.Mutate(p =>
+            {
+                p.Resize(new ResizeOptions
+                {
+                    Size = new Size(800, 600),
+                    Mode = ResizeMode.BoxPad
+                });
+            });
+            using var ms = new MemoryStream();
+            await image.SaveAsWebpAsync(ms);
+            model.Image = ms.ToArray();
+        }
+
+        if (model.ImageFiles is not null)
+        {
+            foreach (var file in model.ImageFiles)
+            {
+                using var image = await Image.LoadAsync(file.OpenReadStream());
+                image.Mutate(p =>
+                {
+                    p.Resize(new ResizeOptions
+                    {
+                        Size = new Size(800, 600),
+                        Mode = ResizeMode.BoxPad
+                    });
+                });
+                using var ms = new MemoryStream();
+                await image.SaveAsWebpAsync(ms);
+                model.ProductImages.Add(new ProductImage
+                {
+                    IsEnabled = true,
+                    CreatedAt = DateTime.UtcNow,
+                    Image = ms.ToArray()
+                });
+            }
+
+        }
+
+
+        if (model.SelectedCatalogs is not null)
+            model.SelectedCatalogs.ToList().ForEach(p => model.Catalogs.Add(dbContext.Catalogs.Find(p)!));
+
+        dbContext.Add(model);
+        await dbContext.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        var item = await dbContext
+            .Products
+            .Include(p => p.Catalogs)
+            .Include(p => p.Specs)
+            .SingleOrDefaultAsync(p => p.Id == id);
+        return View(item);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Product model)
+    {
+        var item = await dbContext.Products.SingleOrDefaultAsync(p => p.Id == model.Id);
+
+        item.Name = model.Name;
+        item.Description = model.Description;
+        item.Price = model.Price;
+        item.BrandId = model.BrandId;
+        item.CategoryId = model.CategoryId;
+        item.Image = model.Image;
+
+        item.IsEnabled = model.IsEnabled;
+
+
+        var form = Request.Form;
+
+        await dbContext.ProductSpecifications.Where(p => p.ProductId == model.Id).ExecuteDeleteAsync();
+
+        var specs = await dbContext.Specifications.ToListAsync();
+
+        specs
+            .Where(p => !string.IsNullOrEmpty(form[p.Id.ToString()]))
+            .ToList()
+            .ForEach(p =>
+            {
+                model.Specs.Add(new ProductSpecification
+                {
+                    SpecificationId = p.Id,
+                    Value = form[p.Id.ToString()]
+                });
+            });
+
+
+        if (model.ImageFile is not null)
+        {
+            using var image = await Image.LoadAsync(model.ImageFile.OpenReadStream());
+            image.Mutate(p =>
+            {
+                p.Resize(new ResizeOptions
+                {
+                    Size = new Size(180, 180),
+                    Mode = ResizeMode.Max
+                });
+            });
+            using var ms = new MemoryStream();
+            await image.SaveAsWebpAsync(ms);
+            item.Image = ms.ToArray();
+        }
+
+        dbContext.Update(item);
+        await dbContext.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var item = await dbContext.Products.SingleOrDefaultAsync(p => p.Id == id);
+        dbContext.Remove(item);
+        await dbContext.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
     }
 }
