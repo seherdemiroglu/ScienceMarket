59069099937ab67c02f3dcc64a2c1331f7a0341b
seherdemiroglu <117183782+seherdemiroglu@users.noreply.github.com>
2025-11-18 12:15:08 +0300

    home page

---
+++ ScienceMarket/AppExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ScienceMarket;
+
+public static class AppExtensions// extensions classlar static olmalı
+{
+    public static string ToSafeUrlString(this string text) => Regex.Replace(string.Concat(text.Where(p => char.IsWhiteSpace(p) || char.IsLetterOrDigit(p)))
+        .ToLower(), @"\s+", "-");
+}
--- ScienceMarket/Controllers/HomeController.cs
+++ ScienceMarket/Controllers/HomeController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ScienceMarketData;
+using Microsoft.EntityFrameworkCore;
 
-namespace ScienceMarket.Controllers
+
+namespace ScienceMarket.Controllers;
+
+public class HomeController (ScienceMarketDbContext dbContext): Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
+
+    public async Task<IActionResult> Category(Guid id)
+    {
+        var model = await dbContext
+            .Categories
+            .Include(p => p.Products)
+            .SingleOrDefaultAsync(p => p.Id == id);
+        return View(model);
+    }
+    public async Task<IActionResult> Catalog(Guid id)
+    {
+        var model = await dbContext
+            .Catalogs
+            .Include(p => p.Products).ThenInclude(p => p.Category)
+            .SingleOrDefaultAsync(p => p.Id == id);
+        return View(model);
+    }
+    public async Task<IActionResult> Brand(Guid id)
+    {
+        var model = await dbContext
+            .Brands
+            .Include(p => p.Products).ThenInclude(p => p.Category)
+            .SingleOrDefaultAsync(p => p.Id == id);
+        return View(model);
+    }
+    public async Task<IActionResult> ProductDetail(Guid id)
+    {
+        var model = await dbContext
+            .Products
+            .Include(p => p.Category)
+            .Include(p => p.Specs).ThenInclude(p => p.Specification)
+            .Include(p => p.Catalogs)
+            .Include(p => p.ProductImages)
+            .Include(p => p.Brand)
+            .SingleOrDefaultAsync(p => p.Id == id);
+        return View(model);
     }
 }
